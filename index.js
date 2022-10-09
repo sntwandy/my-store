6796e42c8b8e6043d8656b44034f94ca116a5aa1
sntwandy <sntwandy@gmail.com>
2022-10-09 12:18:34 -0400

    feat: ✨Installing Express.js and an initial configuration

--- index.js
+++ index.js
@@ -2,4 +2,15 @@
  *
  */
 
-console.log('My App')
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello, my express server!!!');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});

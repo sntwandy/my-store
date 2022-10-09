dc4b82925e5ea4674833346e47cb568a7cb0f140
sntwandy <sntwandy@gmail.com>
2022-10-09 16:01:07 -0400

    feat: ✨Understanding routes

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.get('/', (req, res) => {
 	res.send('Hello, my express server!!!');
 });
 
+app.get('/new-route', (req, res) => {
+	res.send('New route!!!');
+});
+
+app.get('/products', (req, res) => {
+	res.json({
+		id: 1,
+		name: 'Product 1',
+		price: 100,
+	});
+});
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
